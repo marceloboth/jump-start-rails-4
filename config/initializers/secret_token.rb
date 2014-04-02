d94673ecf7ae4dcc02d27a7b5dcbab98694d9fb3
Marcelo J. Both <marcelo.both@gmail.com>
2014-04-01 23:05:10 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jumpstartrails::Application.config.secret_key_base = 'f9698c829f2bc6c78be4ed0c940ec247bc4589192f8949541c3c61993aa55eb33f2a239e3e2dec5a7496ea14f5723c2e8f0b072e9cef5ad86d2e5fd038c7051e'
