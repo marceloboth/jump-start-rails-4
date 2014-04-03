f61dbf71a726d76fa2b16d32c73da641214e3590
Marcelo J. Both <marcelo.both@gmail.com>
2014-04-03 07:27:05 -0300

    Put some validations

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,6 @@
 class Page < ActiveRecord::Base
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :content, presence: true, length: { minimum: 10 }
+  validates :slug, presence: true, format: { with: /\A[a-zA-Z]+\z/,
+    message: "Only letters allowed" }
 end
