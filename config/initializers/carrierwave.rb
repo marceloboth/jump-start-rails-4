3cebf477281d5b9c67f3e161802d10e2de7abe8f
Marcelo J. Both <marcelo.both@gmail.com>
2014-04-15 06:52:15 -0300

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider => 'AWS',
-    :aws_access_key_id => 'AKIAJZWZRTQUONTWUZOA',
-    :aws_secret_access_key => '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'
+    :aws_access_key_id => '',
+    :aws_secret_access_key => ''
   }
 
-  config.fog_directory = 'jumpstartrails_both'
+  config.fog_directory = ''
   config.fog_public    = false
   config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
 end
